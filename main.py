7f1f56051577fa7ec9e7083979d92ea8a4c51316
felixq <felixqx@proton.me>
2026-08-06 00:19:23 +0200

    Add server data management and update functionality
    - Implemented functions to retrieve and save server data from/to servers.json.
    - Added a function to update public server IDs from the Roblox API.
    - Introduced a background thread to periodically update server data every 60 seconds.
    - Updated the .gitignore to exclude servers.json.

--- main.py
+++ main.py
@@ -1,23 +1,71 @@
 import flask
 import requests
 import json
+from threading import Thread
 from dotenv import load_dotenv
+import time
+import os
 
 load_dotenv(".env")
 
 app = flask.Flask(__name__)
 
+public_server_ids = []
+
+
+def get_server_data():
+    f = open("servers.json", "r")
+    data = json.load(f)
+    f.close()
+    return data
+
+def save_server_data(data):
+    f = open("servers.json", "w")
+    json.dump(data, f, indent=4)
+    f.close()
+
+def update_public_servers():
+    url = "https://games.roblox.com/v1/games/11765852158/servers/Public?limit=100"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        public_server_ids.clear()
+        for server in data['data']:
+            public_server_ids.append(server['id'])
+        print(f"Updated public server IDs: {public_server_ids}")
+        return True
+    else:
+        print(f"Failed to update public server IDs. Status code: {response.status_code}")
+        return False
+
 def pull_server_data():
+    update_public_servers()
     url = "https://hydrogen.realisticbwr.org/api/public/servers"
     headers = {
         "User-Agent": "RBWR-Server-Checker/1.0"
     }
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        FO
+        for server in response.json()['data']['servers']:
+            if server['jobId'] not in public_server_ids:
+                continue
+            current_data = get_server_data()
+            if not server['jobId'] in current_data:
+                current_data[server['jobId']] = {}
+
+            current_data[server['jobId']][server['lastHeartbeat']]
+
+            current_data[server['jobId']][server['lastHeartbeat']] = server['state']
+            save_server_data(current_data)
+        return True
     else:
         return None
 
+def update_thread():
+    while True:
+        print("Updating server data...")
+        pull_server_data()
+        time.sleep(60)
 
 @app.route("/api/servers", methods=["GET"])
 def api_servers():
@@ -35,4 +83,7 @@ def style():
 def index():
     return flask.render_template("index.html")
 
+if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+    thread = Thread(target=update_thread, daemon=True)
+    thread.start()
 app.run(host="0.0.0.0", port=5000, debug=True)
